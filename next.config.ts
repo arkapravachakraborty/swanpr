677efda4a7d58356d9d56aee65c7f183291c1330
arkapravachakraborty <arkaprava200212345@gmail.com>
2026-07-06 21:34:55 +0530

    feat: update environment configuration for GitHub, enhance user profile mapping
    the auth flow is done and complete

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['f282-117-214-61-193.ngrok-free.app'],
 };
 
 export default nextConfig;
